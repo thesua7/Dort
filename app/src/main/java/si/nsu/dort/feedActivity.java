1e557481e0571a59a143a8115ec7aeca78232b39
thesua7 <sams.ahamed@northsouth.edu>
2022-04-04 02:30:46 +0600

    card_view structure changed

--- app/src/main/java/si/nsu/dort/feedActivity.java
+++ app/src/main/java/si/nsu/dort/feedActivity.java
@@ -1,27 +1,36 @@
 package si.nsu.dort;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DiffUtil;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.LinearInterpolator;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.lorentzos.flingswipe.SwipeFlingAdapterView;
+import com.yuyakaido.android.cardstackview.CardStackLayoutManager;
+import com.yuyakaido.android.cardstackview.CardStackListener;
+import com.yuyakaido.android.cardstackview.CardStackView;
+import com.yuyakaido.android.cardstackview.Direction;
+import com.yuyakaido.android.cardstackview.StackFrom;
+import com.yuyakaido.android.cardstackview.SwipeableMethod;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class feedActivity extends AppCompatActivity {
 
-    private ArrayList<String> al;
-    private ArrayAdapter<String> arrayAdapter;
-    private int i;
 
-
+    private CardStackLayoutManager manager;
+    private CardStackAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,68 +39,103 @@ public class feedActivity extends AppCompatActivity {
 
 
 
-        al = new ArrayList<>();
-        al.add("php");
-        al.add("c");
-        al.add("python");
-        al.add("java");
-        al.add("html");
-        al.add("c++");
-        al.add("css");
-        al.add("javascript");
-
-        arrayAdapter = new ArrayAdapter<>(this, R.layout.item, R.id.helloText, al );
-        SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView) findViewById(R.id.frame);
-
-        flingContainer.setAdapter(arrayAdapter);
-        flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
+        CardStackView cardStackView = findViewById(R.id.card_stack_view);
+        manager = new CardStackLayoutManager(this, new CardStackListener() {
             @Override
-            public void removeFirstObjectInAdapter() {
-                // this is the simplest way to delete an object from the Adapter (/AdapterView)
-                Log.d("LIST", "removed object!");
-                al.remove(0);
-                arrayAdapter.notifyDataSetChanged();
+            public void onCardDragging(Direction direction, float ratio) {
+                Log.d("OK", "onCardDragging: d=" + direction.name() + " ratio=" + ratio);
             }
 
             @Override
-            public void onLeftCardExit(Object dataObject) {
-                //Do something on the left!
-                //You also have access to the original object.
-                //If you want to use it just cast it (String) dataObject
-                Toast.makeText(feedActivity.this,"Left",Toast.LENGTH_SHORT).show();
+            public void onCardSwiped(Direction direction) {
+                Log.d("OK", "onCardSwiped: p=" + manager.getTopPosition() + " d=" + direction);
+                if (direction == Direction.Right){
+                    Toast.makeText(feedActivity.this, "Direction Right", Toast.LENGTH_SHORT).show();
+                }
+                if (direction == Direction.Top){
+                    Toast.makeText(feedActivity.this, "Direction Top", Toast.LENGTH_SHORT).show();
+                }
+                if (direction == Direction.Left){
+                    Toast.makeText(feedActivity.this, "Direction Left", Toast.LENGTH_SHORT).show();
+                }
+                if (direction == Direction.Bottom){
+                    Toast.makeText(feedActivity.this, "Direction Bottom", Toast.LENGTH_SHORT).show();
+                }
+
+                // Paginating
+                if (manager.getTopPosition() == adapter.getItemCount() - 5){
+                    paginate();
+                }
+
             }
 
             @Override
-            public void onRightCardExit(Object dataObject) {
-                Toast.makeText(feedActivity.this,"Right",Toast.LENGTH_SHORT).show();
+            public void onCardRewound() {
+                Log.d("OK", "onCardRewound: " + manager.getTopPosition());
             }
 
             @Override
-            public void onAdapterAboutToEmpty(int itemsInAdapter) {
-                // Ask for more data here
-                al.add("XML ".concat(String.valueOf(i)));
-                arrayAdapter.notifyDataSetChanged();
-                Log.d("LIST", "notified");
-                i++;
+            public void onCardCanceled() {
+                Log.d("OK", "onCardRewound: " + manager.getTopPosition());
             }
 
             @Override
-            public void onScroll(float scrollProgressPercent) {
-
+            public void onCardAppeared(View view, int position) {
+                TextView tv = view.findViewById(R.id.item_name);
+                Log.d("OK", "onCardAppeared: " + position + ", nama: " + tv.getText());
             }
-        });
 
-
-        // Optionally add an OnItemClickListener
-        flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
             @Override
-            public void onItemClicked(int itemPosition, Object dataObject) {
-                Toast.makeText(feedActivity.this,"Click",Toast.LENGTH_SHORT).show();
+            public void onCardDisappeared(View view, int position) {
+                TextView tv = view.findViewById(R.id.item_name);
+                Log.d("OK", "onCardAppeared: " + position + ", nama: " + tv.getText());
             }
         });
+        manager.setStackFrom(StackFrom.None);
+        manager.setVisibleCount(3);
+        manager.setTranslationInterval(8.0f);
+        manager.setScaleInterval(0.95f);
+        manager.setSwipeThreshold(0.3f);
+        manager.setMaxDegree(20.0f);
+        manager.setDirections(Direction.FREEDOM);
+        manager.setCanScrollHorizontal(true);
+        manager.setSwipeableMethod(SwipeableMethod.Manual);
+        manager.setOverlayInterpolator(new LinearInterpolator());
+        adapter = new CardStackAdapter(addList());
+        cardStackView.setLayoutManager(manager);
+        cardStackView.setAdapter(adapter);
+        cardStackView.setItemAnimator(new DefaultItemAnimator());
+
+    }
+
+    private void paginate() {
+        List<ItemModel> old = adapter.getItems();
+        List<ItemModel> baru = new ArrayList<>(addList());
+        CardStackCallback callback = new CardStackCallback(old, baru);
+        DiffUtil.DiffResult hasil = DiffUtil.calculateDiff(callback);
+        adapter.setItems(baru);
+        hasil.dispatchUpdatesTo(adapter);
+    }
+
+    private List<ItemModel> addList() {
+        List<ItemModel> items = new ArrayList<>();
+        items.add(new ItemModel(R.drawable.rinterestbox, "Markonah", "24", "Jember"));
+        items.add(new ItemModel(R.drawable.rinterestbox, "Marpuah", "20", "Malang"));
+        items.add(new ItemModel(R.drawable.rinterestbox, "Sukijah", "27", "Jonggol"));
+        items.add(new ItemModel(R.drawable.rinterestbox, "Markobar", "19", "Bandung"));
+        items.add(new ItemModel(R.drawable.rinterestbox, "Marmut", "25", "Hutan"));
+
+        items.add(new ItemModel(R.drawable.rinterestbox, "Markonah", "24", "Jember"));
+        items.add(new ItemModel(R.drawable.rinterestbox, "Marpuah", "20", "Malang"));
+        items.add(new ItemModel(R.drawable.rinterestbox, "Sukijah", "27", "Jonggol"));
+        items.add(new ItemModel(R.drawable.rinterestbox, "Markobar", "19", "Bandung"));
+        items.add(new ItemModel(R.drawable.rinterestbox, "Marmut", "25", "Hutan"));
+        return items;
+    }
+
 
     }
 
 
 
-}
+
